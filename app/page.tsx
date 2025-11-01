d0dc5c06a1fdff623d8e1022c9ff1488e38dd835
Jason Y <110353075+JasonY199@users.noreply.github.com>
2025-10-31 20:38:40 -0400

    create initial page structure

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,29 @@
 import Image from "next/image";
 
 export default function Home() {
-  return <h1>Hello World</h1>;
+  return (
+    <article>
+      <Image
+        src="/image-product-mobile.jpg"
+        alt="Product Image"
+        width={700}
+        height={684}
+      />
+      <section>
+        <div>
+          <span>PERFUME</span>
+          <h1>Gabrielle Essence Eau De Parfum</h1>
+          <p>
+            A floral, solar and voluptuous interpretation composed by Olivier
+            Polge, Perfumer-Creator for the House of CHANEL.
+          </p>
+        </div>
+        <div>
+          <span>$149.99</span>
+          <span>$169.99</span>
+        </div>
+        <button>Add to Cart</button>
+      </section>
+    </article>
+  );
 }
