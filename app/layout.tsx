1b8bc91154e857b28dec1a89fee78eba53ee7e28
Jason Y <110353075+JasonY199@users.noreply.github.com>
2025-10-31 20:00:46 -0400

    create new project and import fonts

---
+++ app/layout.tsx
@@ -0,0 +1,36 @@
+import type { Metadata } from "next";
+import { Montserrat, Fraunces } from "next/font/google";
+import "./globals.css";
+
+const montserrat = Montserrat({
+  variable: "--font-montserrat",
+  weight: ["500", "700"],
+  subsets: ["latin"],
+});
+
+const fraunces = Fraunces({
+  variable: "--font-fraunces",
+  weight: ["700"],
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Product Preview",
+  description: "A Frontend Mentor project",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${montserrat.variable} ${fraunces.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
