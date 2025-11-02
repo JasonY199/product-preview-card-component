6b021a853b9230fe41b4239f03898287db8d0acf
Jason Y <110353075+JasonY199@users.noreply.github.com>
2025-11-02 17:27:57 -0500

    style everything except button on mobile

--- app/page.tsx
+++ app/page.tsx
@@ -8,19 +8,22 @@ export default function Home() {
         alt="Product Image"
         width={700}
         height={684}
+        className="rounded-t-lg"
       />
       <section className="flex flex-col gap-6 bg-white p-8">
         <div className="flex flex-col gap-6">
-          <span>PERFUME</span>
-          <h1>Gabrielle Essence Eau De Parfum</h1>
-          <p>
+          <span className="text-preset-4">PERFUME</span>
+          <h1 className="text-preset-1 text-black">
+            Gabrielle Essence Eau De Parfum
+          </h1>
+          <p className="text-preset-3">
             A floral, solar and voluptuous interpretation composed by Olivier
             Polge, Perfumer-Creator for the House of CHANEL.
           </p>
         </div>
-        <div>
-          <span>$149.99</span>
-          <span>$169.99</span>
+        <div className="flex gap-4 items-center">
+          <span className="text-preset-1 text-green-500">$149.99</span>
+          <span className="text-preset-5 line-through">$169.99</span>
         </div>
         <button>Add to Cart</button>
       </section>
